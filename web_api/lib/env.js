7c4a80ecaa525341be1997463db2d87d8124ba4f
decodxr <155329552+decodxr@users.noreply.github.com>
2026-05-09 23:31:07 -0300

    Update env.js

--- web_api/lib/env.js
+++ web_api/lib/env.js
@@ -3,11 +3,11 @@
 require("dotenv").config();
 
 exports.env = {
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
-  GEMINI_API_KEY_2: process.env.GEMINI_API_KEY_2 || "",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDZd53oWROQb-uvYMpSHmUnr1Pm7wRty3A",
+  GEMINI_API_KEY_2: process.env.GEMINI_API_KEY_2 || "AIzaSyDE1MLa316MMxQhk-dN5Oz6-5psVkipot4",
   GEMINI_MODEL: process.env.GEMINI_MODEL || "gemini-2.5-flash",
   GEMINI_BASE_URL: process.env.GEMINI_BASE_URL || "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
-  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || "",
+  OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY || "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",
   OPENROUTER_MODEL: process.env.OPENROUTER_MODEL || "deepseek/deepseek-chat",
   OPENROUTER_BASE_URL: process.env.OPENROUTER_BASE_URL || "https://openrouter.ai/api/v1/chat/completions"
 };
